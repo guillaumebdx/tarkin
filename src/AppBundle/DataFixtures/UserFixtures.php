a5d123e12b0e1104845ec130a93290a8c6dc5aa5
Guillaume HARARI <guillaumeharari@hotmail.com>
2018-12-09 01:12:07 +0100

    Add DataFixtures

---
+++ src/AppBundle/DataFixtures/UserFixtures.php
@@ -0,0 +1,34 @@
+<?php 
+namespace AppBundle\DataFixtures;
+
+use AppBundle\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\ORMFixtureInterface;
+
+class UserFixtures extends Fixture implements ORMFixtureInterface
+
+{
+
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setEmail('guillaumeharari@hotmail.com');
+        $user->setEmailCanonical('guillaumeharari@hotmail.com');
+        $user->setNameReference('Harari');
+        $user->setPassword('root');
+        $manager->persist($user);
+        
+        $user = new User();
+        $user->setEmail('henridupont@hotmail.com');
+        $user->setEmailCanonical('henridupont@hotmail.com');
+        $user->setNameReference('Dupont');
+        $user->setPassword('root');
+        $manager->persist($user);
+        
+        $manager->flush();
+    }
+    
+    
+}
